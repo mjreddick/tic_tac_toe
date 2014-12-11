149b1446fae0150fb73e4bd079c1f1bdf6f73d91
Matthew Reddick <matthewjreddick@gmail.com>
2014-12-10 20:19:44 -0800

    removed extra commented out code

--- js/GameBoard.js
+++ js/GameBoard.js
@@ -16,20 +16,13 @@
 			var self = this;
 
 			//properties
-			// self.minorBoards = new Array(9);
-			// self.currentPlayer = null;
-			// self.gameState = null;
-
 			self.gameState = {minorBoards: new Array(9),
 							currentPlayer: null,
 							gameStatus: null};
 			
 
 			//methods
-			// self.init = init;
-			// self.resetBoard = resetBoard;
 			self.makeMove = makeMove;
-			// self.moveMade = moveMade;
 			self.checkForWin = checkForWin;
 			self.changePlayer = changePlayer;
 			self.getPlayerPiece = getPlayerPiece;
@@ -38,8 +31,6 @@
 			self.setActiveState = setActiveState;
 
 			//initialization
-
-			
 			(function init() {
 				for(var i = 0; i < self.gameState.minorBoards.length; i++) {
 						self.gameState.minorBoards[i] = new MinorBoard();
@@ -47,14 +38,9 @@
 					}
 				self.gameState.currentPlayer = 0;
 				self.gameState.gameStatus = 0;
-
-				// this.gameState = 0;
-				// currentPlayer = 0;
-
 			})()
 
 			//method declarations
-
 			function makeMove(boardIndex, cellIndex) {
 				if(self.gameState.minorBoards[boardIndex].cells[cellIndex] === 0 
 					&& self.gameState.minorBoards[boardIndex].active 
@@ -62,7 +48,6 @@
 				{
 					self.gameState.minorBoards[boardIndex].cells[cellIndex] = self.getPlayerPiece();
 					self.gameState.minorBoards[boardIndex].status = self.checkForWin(self.gameState.minorBoards[boardIndex].cells);// might need two win checkers
-					// self.parentBoard.moveMade(cellPosition);
 					self.gameState.gameStatus = self.checkForWin(self.gameState.minorBoards);
 					self.changePlayer();
 					self.setActiveState(cellIndex);
@@ -112,43 +97,9 @@
 				return PLAYER_PIECE[self.gameState.currentPlayer]
 			}
 
-			// function resetBoard() {
-			// 	for(var i = 0; i < self.cells.length; i++) {
-			// 			self.cells[i].init();
-			// 			self.cells[i].active = 1;
-			// 		}
-			// 	self.gameState = 0;
-			// 	currentPlayer = 0;
-			// }
-
 			function changePlayer() {
 				self.gameState.currentPlayer = (self.gameState.currentPlayer + 1) % PLAYER_PIECE.length;
 			}
-
-			// function moveMade(index) {
-			// 	self.gameState = self.checkForWin(self.cells); 
-			// 	self.changePlayer();
-			// 	if(self.cells[index].isPlayable()) {
-			// 		//set all cells boardActive to false
-			// 		//except cells[index] which gets set to true
-			// 		for(var i = 0; i < self.cells.length; i++) {
-			// 			if(i === index) {
-			// 				self.cells[i].setActive(true);
-			// 			}
-			// 			else {
-			// 				self.cells[i].setActive(false);
-			// 			}
-			// 		}
-			// 	}
-			// 	else {
-			// 		//set all cells boardActive to true
-			// 		for(var i = 0; i < self.cells.length; i++) {
-			// 			self.cells[i].setActive(true);
-			// 		}
-
-			// 	}
-			// }
-
 
 			function checkForWin(arr) {
 				//returns a 0 for no win, 1 for X win, 2 for O win, and 3 for cat's game
@@ -167,31 +118,23 @@
 
 				for(var i = 0; i < 3; i++) {
 					//check verticle
-					if(		cells[i]		=== cells[i + 3]
-						&& 	cells[i + 3]	=== cells[i + 6] 
-						&& 	cells[i]		!== 0) 
+					if(cells[i] === cells[i + 3] && cells[i + 3] === cells[i + 6] && cells[i] !== 0) 
 					{
 						return cells[i];
 					}
 					//check horizontal
 					var j = i * 3;
-					if(		cells[j]		=== cells[j + 1] 
-						&& 	cells[j + 1]	=== cells[j + 2] 
-						&& 	cells[j]		!== 0) 
+					if(cells[j] === cells[j + 1] && cells[j + 1] === cells[j + 2] && cells[j] !== 0) 
 					{
 						return cells[j];
 					}
 				}
 				//check diagonals
-				if(		cells[0] === cells[4] 
-					&&	cells[4] === cells[8] 
-					&&	cells[0] !== 0)
+				if(cells[0] === cells[4] &&	cells[4] === cells[8] && cells[0] !== 0)
 				{
 					return cells[0];
 				}
-				if(		cells[2] === cells[4] 
-					&&	cells[4] === cells[6] 
-					&&	cells[2] !== 0) 
+				if(cells[2] === cells[4] &&	cells[4] === cells[6] && cells[2] !== 0) 
 				{
 					return cells[2];
 				}
--- js/MinorBoard.js
+++ js/MinorBoard.js
@@ -4,93 +4,18 @@
 		.factory('MinorBoard', MinorBoardFunc);
 
 	function MinorBoardFunc() {
-		//0 = blank space, 1 = X, 2 = O
-		// state: 0 no winner, 1 X wins, 2 O wins
-		
-		
+		// in cells: 0 = blank space, 1 = X, 2 = O
+		// status: 0 no winner, 1 X wins, 2 O wins		
 
 		var MinorBoard = function() {//(parentBoard, size) {
-			//private variables
-			// var self = this;
 
 			//properties
+			// 0 = blank space, 1 = X, 2 = O
 			this.cells = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-			// self.currentPlayer = 0;
+			// 0 no winner, 1 X wins, 2 O wins
 			this.status = 0;
-			// this.parentBoard = parentBoard;
+			//keeps track of whether or not the board can be played on
 			this.active = true;
-			//methods
-			// self.makeMove = makeMove;
-			// self.init = init;
-			// // self.getPlayerPiece = getPlayerPiece;
-			// // self.changeTurn = changeTurn;
-			// self.getCellState = getCellState;
-			// self.checkForWin = checkForWin;
-			// self.getGameMessage = getGameMessage;
-			// self.isPlayable = isPlayable;
-			// self.setActive = setActive;
-			// self.valueOf = valueOf;
-
-			// self.init();
-
-			// function init() {
-			// 	for(var i = 0; i < self.cells.length; i++) {
-			// 		self.cells[i] = 0;
-			// 	}
-			// }
-
-			// function valueOf() {
-			// 	return self.state;
-			// }
-
-			// function checkForWin() {
-			// 	if(self.state === 0){
-			// 		return self.parentBoard.checkForWin(self.cells);
-			// 	}
-			// 	return self.state;
-
-			// }
-
-			// function makeMove(cellPosition) {
-			// 	if(self.cells[cellPosition] === 0 && self.active && self.parentBoard.gameState === 0) {
-			// 		self.cells[cellPosition] = self.parentBoard.getPlayerPiece();
-			// 		self.state = self.checkForWin();
-			// 		self.parentBoard.moveMade(cellPosition);
-			// 	}
-			// }
-
-			// // function getPlayerPiece() {
-			// // 	return self.parentBoard.getPlayerPiece();
-			// // }
-
-			
-
-			// function getCellState(cellPosition) {
-			// 	return CELL_STATE[self.cells[cellPosition]];
-			// }
-
-			
-			// function getGameMessage() {
-			// 	return MESSAGES[self.gameState];
-			// }
-
-			// function isPlayable() {
-			// 	for(var i = 0; i < self.cells.length; i++) {
-			// 		if(self.cells[i] === 0) {
-			// 			return true;
-			// 		}
-			// 	}
-			// 	return false;
-			// }
-
-			// function setActive(bool) {
-			// 	if(bool && self.isPlayable()) {
-			// 		self.active = true;
-			// 	}
-			// 	else{
-			// 		self.active = false;
-			// 	}
-			// }
 		}
 
 		return MinorBoard;
